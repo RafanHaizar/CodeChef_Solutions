f9d18ddadf6043cf587d7ac572bd5825acffd50f
RafanHaizar <148255036+RafanHaizar@users.noreply.github.com>
2025-06-23 20:08:30 +0600

    Create SQUIDBANK.cpp

---
+++ SQUIDBANK.cpp
@@ -0,0 +1,26 @@
+/**
+ * @tagline Bismillahi Allahuakbar, JOY BANGLA!!!
+ * @author Rafan
+ * OIA IOAAA OIIIII
+ *             - Cat
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef double dbl;
+typedef string str;
+
+str output_header = " ----OUTPUT----- ";
+str spp = " ";
+str yes = "YES";
+str no = "NO";
+vector<long long int> dp;
+
+int main() {
+	int a,b;
+	cin >> a>>b;
+	cout << (a-b)*10000;
+}
