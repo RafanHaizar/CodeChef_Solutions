66f29765d8a5eed0f4405f8ed00d1ae32b654001
RafanHaizar <148255036+RafanHaizar@users.noreply.github.com>
2025-06-23 22:24:53 +0600

    Create INPL.cpp

---
+++ INPL.cpp
@@ -0,0 +1,34 @@
+/**
+ * @tagline Bismillahi Allahuakbar, JOY BANGLA!!!
+ * @author Rafan
+ * OIA IOAAA OIIIII
+ *             - Cat
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef double dbl;
+typedef string str;
+
+str output_header = " ----OUTPUT----- ";
+str spp = " ";
+str yes = "Yes";
+str no = "No";
+vector < long long int > dp;
+
+#define opp_endl(condition)((condition) ? (cout << yes << endl << flush) : (cout << no << endl << flush))
+#define opp(condition)((condition) ? (cout << yes << flush) : (cout << no << flush))
+
+int main() {
+    int n;
+    cin >> n;
+    if (n > 6) {
+        cout << "THALA";
+    } else {
+        cout << "BOOM";
+    }
+    return 0;
+}
