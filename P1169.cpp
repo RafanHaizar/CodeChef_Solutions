30edf9ce15fbe13497c1387f4abd8f4ee4daaa42
RafanHaizar <148255036+RafanHaizar@users.noreply.github.com>
2025-06-23 20:11:37 +0600

    Create P1169.cpp

---
+++ P1169.cpp
@@ -0,0 +1,30 @@
+/**
+ * @tagline Bismillahi Allahuakbar, JOY BANGLA!!!
+ * @author Rafan
+ * OIA IOAAA OIIIII
+ *             - Cat
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef double dbl;
+typedef string str;
+
+str output_header = " ----OUTPUT----- ";
+str spp = " ";
+str yes = "YES";
+str no = "NO";
+vector<long long int> dp;
+
+int main() {
+	int a;
+	cin >>a;
+	if(a>9){
+	    cout << "yes";
+	}else{
+	    cout << "no";
+	}
+}
