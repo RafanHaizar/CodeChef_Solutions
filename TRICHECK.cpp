5b825cd68a19fc60cc1e256e1937ef0624a1615b
RafanHaizar <148255036+RafanHaizar@users.noreply.github.com>
2025-06-23 20:09:46 +0600

    Create TRICHECK.cpp

---
+++ TRICHECK.cpp
@@ -0,0 +1,30 @@
+/**
+ * @tagline Bismillahi Allahuakbar, JOY BANGLA!!!
+ * @author Rafan
+ * OIA IOAAA OIIIII
+ *             - Cat
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef double dbl;
+typedef string str;
+
+str output_header = " ----OUTPUT----- ";
+str spp = " ";
+str yes = "YES";
+str no = "NO";
+vector<long long int> dp;
+
+int main() {
+	int a,b,c;
+	cin >> a>>b>>c;
+	if(a+b>c&&b+c>a&&a+c>b){
+	    cout <<"Yes";
+	}else{
+	    cout<<"No";
+	}
+}
