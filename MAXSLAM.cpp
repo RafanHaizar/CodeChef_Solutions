60c0ff0d2ccf23bbd3da40c49b3b38c75b39eb9b
RafanHaizar <148255036+RafanHaizar@users.noreply.github.com>
2025-06-24 08:47:18 +0600

    Create MAXSLAM.cpp

---
+++ MAXSLAM.cpp
@@ -0,0 +1,30 @@
+/**
+ * @tagline Bismillahi Allahuakbar, JOY BANGLA!!!
+ * @author Rafan
+ * OIA IOAAA OIIIII
+ *             - Cat
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef double dbl;
+typedef string str;
+
+str output_header = " ----OUTPUT----- ";
+str spp = " ";
+str yes = "Yes";
+str no = "No";
+vector < long long int > dp;
+
+#define opp_endl(condition)((condition) ? (cout << yes << endl << flush) : (cout << no << endl << flush))
+#define opp(condition)((condition) ? (cout << yes << flush) : (cout << no << flush))
+
+int main() {
+    int a;
+    cin >> a;
+    cout << ceil((double)(double)(25 - a) / 4);
+    return 0;
+}
