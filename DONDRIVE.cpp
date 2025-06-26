293abd248a021076980ebbb08d64e588063d262b
RafanHaizar <148255036+RafanHaizar@users.noreply.github.com>
2025-06-26 09:09:24 +0600

    Create DONDRIVE.cpp

---
+++ DONDRIVE.cpp
@@ -0,0 +1,39 @@
+/**
+ * @tagline Bismillahi Allahuakbar, JOY BANGLA!!!
+ * @author Rafan
+ * OIA IOAAA OIIIII
+ *             - Cat
+ * Free Palestine!
+ * May Allah help our brothers and sisters all around the globe. Amin.
+ */
+
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+typedef long long ll;
+typedef double dbl;
+typedef string str;
+
+str output_header = " ----OUTPUT----- ";
+str spp = " ";
+str yes = "YES";
+str no = "NO";
+vector < long long int > dp;
+
+#define opp_endl(condition)((condition) ? (cout << yes << endl << flush) : (cout << no << endl << flush))
+#define opp(condition)((condition) ? (cout << yes << flush) : (cout << no << flush))
+
+vector < int > sieve;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << a - b << endl;
+    }
+    return 0;
+}
