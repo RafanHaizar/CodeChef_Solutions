d3788e7b60ca0119f3da64f6d0f7dd229020c73c
RafanHaizar <148255036+RafanHaizar@users.noreply.github.com>
2025-06-26 08:05:10 +0600

    Create ERROR404.cpp

---
+++ ERROR404.cpp
@@ -0,0 +1,35 @@
+/**
+ * @tagline Bismillahi Allahuakbar, JOY BANGLA!!!
+ * @author Rafan
+ * OIA IOAAA OIIIII
+ *             - Cat
+ * Free Palestine!
+ * May Allah help our brothers and sisters all around the globe. Amin.
+ */
+
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+typedef long long ll;
+typedef double dbl;
+typedef string str;
+
+str output_header = " ----OUTPUT----- ";
+str spp = " ";
+str yes = "NOT FOUND";
+str no = "FOUND";
+vector < long long int > dp;
+
+#define opp_endl(condition)((condition) ? (cout << yes << endl << flush) : (cout << no << endl << flush))
+#define opp(condition)((condition) ? (cout << yes << flush) : (cout << no << flush))
+
+vector < int > sieve;
+
+int main() {
+    int n;
+    cin >> n;
+    opp(n == 404);
+    return 0;
+}
